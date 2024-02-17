e85bec23c637c072e00cc1427dea8ca776c2f810
AleksandrMelexin <Melehin-Sasha7@yandex.ru>
2024-02-17 22:58:09 +0300

    написан код к работе 1

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,3 +1,4 @@
+#задача с смешиванием газов варинат 14
 import math
 
 
@@ -6,11 +7,11 @@ def main():
     P = 101325
     Va = 4 * pow(10, -4)  # азот N2
     Vb = 7 * pow(10, -4)  # вода H2O
-    R = 8.31
+    R = 8.3144
     na = (P * Va) / (R * T)
     nb = (P * Vb) / (R * T)
     answer = na * R * math.log((Va + Vb) / Va) + nb * R * math.log((Va + Vb) / Vb)
-    print("изменение энтропии: ", round(answer, 4))
+    print("Изменение энтропии: ", round(answer, 4), "Дж/(Моль*К)")
 
 
 if __name__ == "__main__":
---
+++ lab1/task3.py
@@ -0,0 +1,21 @@
+# расчёт энтропии вариант 14
+import math
+
+
+def S(array, T):
+    R = 8.31
+    result = array[0] * math.log(T)
+    for i in range(1, 5):
+        result += (array[i]/i)*math.pow(T, i)
+    result += array[6]
+    return result * R
+        
+    
+def main():
+    T = 250
+    BLUE_C3H4 = [0.26130445E+01, 0.12122575E-01, 0.18539880E-04, 0.12791347E-07, -0.10482247E-10, 0.21541567E+05, 0.10226139E+02] # Third Milenium стр. 68 pdf
+    print("Энтропия вещества C3H4 равна:", round(S(BLUE_C3H4, T), 4), "Дж/(Моль*К)")
+
+
+if __name__ == "__main__":
+    main()
