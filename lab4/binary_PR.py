c5ec547d2f7ffd013e8b94a09fe4d09bb3ecf44b
AleksandrMelexin <Melehin-Sasha7@yandex.ru>
2024-03-28 00:26:35 +0300

    работа 4 отправлена

--- lab4/binary_PR.py
+++ lab4/binary_PR.py
@@ -1,34 +1,24 @@
 from thermo import ChemicalConstantsPackage, CEOSGas, CEOSLiquid, PRMIX, FlashVL, FlashVLN, FlashPureVLS
 from thermo.interaction_parameters import IPDB
-# Load constants and properties
-constants, properties = ChemicalConstantsPackage.from_IDs(['acetone', '1-butanol'])
-# Objects are initialized at a particular condition
-T = 372
-P = 1e5
-zs = [.5, .5]
 
-# Use Peng-Robinson for both the vapor and the liquid phases
+
+constants, properties = ChemicalConstantsPackage.from_IDs(['acetone', '1-butanol']) # данные вещества
+T = 375.51 # температура
+P = 2e5 # давление
+zs = [.5, .5] # мольные доли
+
 k12 = 0.03
 kijs = [[0, k12],
         [k12, 0]]
-print(k12)
+
 eos_kwargs = dict(Tcs=constants.Tcs, Pcs=constants.Pcs, omegas=constants.omegas, kijs=kijs)
 gas = CEOSGas(PRMIX, eos_kwargs, HeatCapacityGases=properties.HeatCapacityGases, T=T, P=P, zs=zs)
 liquid = CEOSLiquid(PRMIX, eos_kwargs, HeatCapacityGases=properties.HeatCapacityGases, T=T, P=P, zs=zs)
-#gas = CEOSGas(PRMIX, eos_kwargs=eos_kwargs)
-#liquid = CEOSLiquid(PRMIX, eos_kwargs=eos_kwargs)
-
-# Create a flasher instance, assuming only vapor-liquid behavior
 flasher = FlashVL(constants, properties, liquid=liquid, gas=gas)
 
-# Create a T-xy plot at P bar
-_ = flasher.plot_Txy(P=P, pts=100)
-
-# Create a P-xy plot at T Kelvin
-_ = flasher.plot_Pxy(T=T, pts=100)
-
-# Create a xy diagram at T Kelvin
-_ = flasher.plot_xy(T=T, pts=100)
+_ = flasher.plot_Txy(P=P, pts=100) # создание Tx-y диаграммы
+_ = flasher.plot_Pxy(T=T, pts=100) # создание Px-y диаграммы
+_ = flasher.plot_xy(T=T, pts=100) # создание x-y диаграммы
 
 liquid2 = CEOSLiquid(PRMIX, eos_kwargs, HeatCapacityGases=properties.HeatCapacityGases, T=T, P=P, zs=zs)
 flasher2 = FlashVLN(constants, properties, liquids=[liquid, liquid2], gas=gas)
@@ -36,7 +26,7 @@ res = flasher2.flash(T=T, P=P, zs=zs)
 print('There are %s phases present at %f K and %f bar' %(res.phase_count,T,P/1e5))
 if res.VF > 0:
 	print(res.gas.zs)
-if res.VF == 1:  # Есть только пар
+if res.VF == 1:  
 	print("Only vapour")
 else:
 	print("Liquid0: ")
--- lab4/example/example.py
+++
@@ -1,8 +0,0 @@
-flasher = FlashVL(constants, properties, liquid=liquid, gas=gas)
-res = flasher.flash(T=360, P=P, zs=[0.2, 0.8])
-if res.VF > 0:
-  print("Vapour exists")
-  print(res.gas.zs)
-if res.VF < 1:
-  print("Liquid exists")
-  print(res.liquid0.zs)
